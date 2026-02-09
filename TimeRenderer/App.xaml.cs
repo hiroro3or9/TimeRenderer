71af7fd6057e3ac41db5f35ee4604013698ac4a0
hiroro3or9 <hiroki_02130307@yahoo.co.jp>
2026-02-09 20:09:54 +0900

    プロジェクトファイルを追加。

---
+++ TimeRenderer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TimeRenderer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
